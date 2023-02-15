30ff66b0f881422f757e18599b9e28e0af7ce591
kole ervine <koleervine@gmail.com>
2023-02-15 10:07:38 -0700

    removed useless comments

--- personalityQuiz/personalityQuiz/ResultsViewController.swift
+++ personalityQuiz/personalityQuiz/ResultsViewController.swift
@@ -1,9 +1,3 @@
-//
-//  ResultsViewController.swift
-//  personalityQuiz
-//
-//  Created by kole ervine on 2/13/23.
-//
 
 import UIKit
 
@@ -14,16 +8,4 @@ class ResultsViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- personalityQuiz/personalityQuiz/ViewController.swift
+++ personalityQuiz/personalityQuiz/ViewController.swift
@@ -1,19 +1,8 @@
-//
-//  ViewController.swift
-//  personalityQuiz
-//
-//  Created by kole ervine on 2/2/23.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
